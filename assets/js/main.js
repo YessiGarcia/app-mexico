73045241d3d6893db8f420c1f7cd774feb2ecc65
Miriam Elizabeth Laguna Velazquez <Miriam Elizabeth Laguna Velazquez>
2017-05-05 11:29:12 -0500

    javascript

---
+++ assets/js/main.js
@@ -0,0 +1,18 @@
+var espacioImagen = document.getElementById("modal");
+var imgMostrar = document.getElementById("imagenNueva");
+var imagenes = document.getElementsByClassName("imgMx");
+var span = document.getElementById("close");
+
+span.addEventListener("click",cerrar);
+
+for(var i=0;i<imagenes.length;i++){
+  var listaImagenes = imagenes[i].addEventListener("click",mostrar);
+}
+
+function mostrar(){
+  espacioImagen.style.display="block";
+  imgMostrar.src=this.src;
+};
+function cerrar(){
+  espacioImagen.style.display="none";
+};
