52804a03d7d8cf2154374ab0baf18566107553c9
magentaFire <dafne.magenta@gmail.com>
2017-05-17 09:39:28 -0500

    modificados estilos para modal

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,8 +2,14 @@ var espacioImagen = document.getElementById("modal");
 var imgMostrar = document.getElementById("imagenNueva");
 var imagenes = document.getElementsByClassName("imgMx");
 var span = document.getElementById("close");
+var spans = document.getElementsByTagName("span");
+var texto = document.getElementById("textoNuevo");
 
 span.addEventListener("click",cerrar);
+
+for (var i = 0; i > spans.length; i++) {
+  var listaTexto = span[i].addEventListener("click",mostrar)
+}
 
 for(var i=0;i<imagenes.length;i++){
   var listaImagenes = imagenes[i].addEventListener("click",mostrar);
@@ -11,7 +17,9 @@ for(var i=0;i<imagenes.length;i++){
 
 function mostrar(){
   espacioImagen.style.display="block";
+  texto.style.display="block";
   imgMostrar.src=this.name;
+  spans.src=this.name;
 };
 function cerrar(){
   espacioImagen.style.display="none";
